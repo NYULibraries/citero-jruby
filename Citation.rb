b1dc2c62e2320959f500c651d319e19d60e6a6af
hab278 <as>
2012-09-24 16:13:22 -0400

    Revert "back to basics"
    This reverts commit a6fc3936e4c45ba7c81864cf4db54ea4471c23c9.

--- Citation.rb
+++ Citation.rb
@@ -1,6 +1,9 @@
 require 'java'
 Dir[ENV['CITATION'] + '/*.jar'].each {|f| require f}
-Dir[ENV['M2_HOME'] + '/repository/**/*.jar'].each { |f| require f }
+Dir[ENV['M2_HOME'] + '/repository/**/*.jar'].each do |f|
+  puts f
+  require f
+end
 java_import Java::EduNyuLibraryCitation::Citation
 java_import Java::edu.nyu.library.citation.Formats  
 tool = Citation.new("itemType: journalArticle\nauthor: Me",Formats::CSF).output(Formats::OPENURL)
