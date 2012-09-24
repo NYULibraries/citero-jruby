80e32ccab09b5ebb8aa8b12ccc355c288ed3c236
hab278 <as>
2012-09-24 16:13:22 -0400

    Revert "sample on how to use citation in JRuby"
    This reverts commit 007206ac9d3f513d558cbbd497e84c64e06e3da0.

--- Citation.rb
+++
@@ -1,7 +0,0 @@
-require 'java'
-require ENV['CITATION']
-Dir[ENV['M2_HOME'] + '/repository/**/*.jar'].each { |f| require f }
-java_import Java::EduNyuLibraryCitation::Citation
-java_import Java::edu.nyu.library.citation.Formats  
-tool = Citation.new("itemType: journalArticle\nauthor: Me",Formats::CSF).output(Formats::OPENURL)
-puts tool
