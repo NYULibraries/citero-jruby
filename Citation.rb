a22ca3e16512856d96c741630fd11d51d319c3b9
hab278 <as>
2012-09-24 16:13:22 -0400

    Revert "environment vars for require"
    This reverts commit 1866c79969c3068066ce8915114d5cf1f80655fb.

--- Citation.rb
+++ Citation.rb
@@ -1,5 +1,5 @@
 require 'java'
-require ENV['CITATION'] + '/*.jar'
+require ENV['CITATION']
 Dir[ENV['M2_HOME'] + '/repository/**/*.jar'].each { |f| require f }
 java_import Java::EduNyuLibraryCitation::Citation
 java_import Java::edu.nyu.library.citation.Formats  
